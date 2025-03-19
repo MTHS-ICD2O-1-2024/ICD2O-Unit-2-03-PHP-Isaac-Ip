f2e8d381ac9a4dbda8cbf6a5199c60b7333095c8
default <isaac.ip@stu.ocsb.ca>
2025-03-19 01:07:37 +0000

    changed button

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
           <button
             class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
             type="submit">
-            Answer
+            Enter
           </button>
         </form>
       </div>
